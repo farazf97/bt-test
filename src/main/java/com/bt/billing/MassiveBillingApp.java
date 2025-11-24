bfed619b43bb103d2a745713310ad21464476bf8
Faraz <faraz_f97@yahoo.com>
2025-11-24 19:05:55 +0000

    feat: Initialize Bt-test project with a main class, and essential build configuration.

---
+++ src/main/java/com/bt/billing/MassiveBillingApp.java
@@ -0,0 +1,8 @@
+package main.java.com.bt.billing;
+
+public class MassiveBillingApp {
+
+    public static void main(String[] args) {
+        System.out.println("MassiveBillingApp started");
+    }
+}
